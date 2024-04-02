022a39ea64f520ca36d3c72cefd5fcc167277d7e
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-04-02 23:15:04 +0530

    clerk

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-providers";
+import { ClerkProvider } from '@clerk/nextjs'
 
 
 const font = DM_Sans({ subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={font.className}>
       <ThemeProvider
@@ -29,5 +31,6 @@ export default function RootLayout({
           </ThemeProvider>
         </body>
     </html>
+    </ClerkProvider>
   );
 }
