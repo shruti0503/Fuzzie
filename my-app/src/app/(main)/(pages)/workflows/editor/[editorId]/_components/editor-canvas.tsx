052c862613cded20c500b6745ae5199f3359aca0
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-04-10 22:16:52 +0530

    canvas

--- my-app/src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
+++ my-app/src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
@@ -1,10 +1,56 @@
 'use client'
+import { EditorCanvasCardType, EditorNodeType } from '@/lib/types'
+import { useEditor } from '@/providers/editor-provider'
+import React , {useCallback , useEffect, useMemo , useState} from 'react'
+import ReactFlow, { Background ,
+    Connection,
+    Controls,
+    Edge,
+    EdgeChange,
+    MiniMap,
+    NodeChange,
+    ReactFlowInstance,
+    applyNodeChanges,
+    applyEdgeChanges,
+    addEdge
 
-import React from 'react'
+} from 'reactflow'
+
+import 'reactflow/dist/style.css'
+import EditorCanvasCardSingle from './editor-canvas-card'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
+import { toast } from 'sonner'
+import { usePathname } from 'next/navigation'
+import { v4 as uuidv4 } from 'uuid';
+
 
 type Props = {}
+const initialNodes:EditorNodeType[]=[]
+const initialEdges:{id:string, source:string, target:string}[]=[]
 
-function EditorCanvas({}: Props) {
+function EditorCanvas(props: Props) {
+
+
+    const {dispatch,state}=useEditor();
+    const[ nodes, setNodes]=useState(initialNodes)
+    const [edges, setEdges]=useState(initialEdges)
+    const [isWorkFlowInstance, setIsWorkFlowLoading]=useState<boolean>(false)
+    const [ReactFlowInstance, setReactFlowInstance]=useState<ReactFlowInstance>()
+    const pathname=usePathname()
+    
+    // to handle the dragover event. 
+    //triggered when a draggable element is being dragged over a drop target, in this case, the editor canvas.
+    const onDragOver = useCallback((event: any) => { //This object contains information about the dragover event, such as the coordinates of the mouse pointer.
+        event.preventDefault()
+        console.log("onDragPver", event)
+        event.dataTransfer.dropEffect = 'move'
+      }, []) //he onDragOver function ensures that the default behavior of disallowing dropping is prevented during a dragover event on the editor canvas. It also specifies the drop effect as 'move' to indicate that the draggable element can be moved when dropped onto the canvas.
+
+      
+
+
+
+
   return (
     <div>EditorCanvas</div>
   )
